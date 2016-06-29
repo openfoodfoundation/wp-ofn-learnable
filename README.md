98a114ba12628ebcc7d2207c444b734eab73911e
Rohan Mitchell <rohan@rohanmitchell.com>
2016-06-29 11:57:34 +1000

    Add initial learnable content type and README with setup instructions

---
+++ README.md
@@ -0,0 +1,16 @@
+# Learnable
+
+This WordPress plugin creates a post type Learnable, which is used to display a number of
+learning resources in a tile view (via shortcode).
+
+
+## Dependencies
+
+- Advanced Custom Fields - https://www.advancedcustomfields.com/
+
+
+## Installation
+
+1. Install the plugin
+2. Install Advanced Custom Fields
+3. Add a custom field 'Link', applying only to post type 'ofn_learnable'
---
+++ ofn-learnable.php
@@ -0,0 +1,43 @@
+<?php
+/*
+  Plugin Name: OFN Learnable
+  Description: Manage a tile index of resources, stories and models
+  Version: 0.0.1
+  Author: Rohan Mitchell
+  Author URI: https://github.com/RohanM
+  Plugin URI: http://openfoodnetwork.org
+*/
+/*  Copyright 2016 Open Food Foundation
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+
+add_action( 'init', 'create_post_type' );
+function create_post_type() {
+  register_post_type( 'ofn_learnable',
+    array(
+      'labels' => array(
+        'name' => __( 'Learnables' ),
+        'singular_name' => __( 'Learnable' )
+      ),
+      'public' => true,
+      'has_archive' => true,
+      'rewrite' => array('slug' => 'learnable'),
+      'taxonomies' => array('category', 'post_tag'),
+      'supports' => array('title', 'thumbnail', 'revisions'),
+    )
+  );
+}
