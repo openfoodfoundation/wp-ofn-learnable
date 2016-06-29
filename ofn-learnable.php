d5f4da7a3aa83e97a12806526490de941c711ad5
Rohan Mitchell <rohan@rohanmitchell.com>
2016-06-29 12:34:23 +1000

    Add shortcode to display a learnable in HTML

--- ofn-learnable.php
+++ ofn-learnable.php
@@ -41,3 +41,42 @@ function create_post_type() {
     )
   );
 }
+
+
+function ofn_learnable_all($atts) {
+    $html = '';
+
+    $args = array('post_type' => 'ofn_learnable');
+    $loop = new WP_Query($args);
+
+    while($loop->have_posts()) {
+        $loop->the_post();
+
+        $title = get_the_title();
+        $image = wp_get_attachment_url(get_post_thumbnail_id());
+        $category = get_the_category()[0]->name;
+        $link = get_field('link');
+
+        $html .= ofn_learnable_as_html($title, $image, $category, $link);
+    }
+
+    return $html;
+}
+add_shortcode('learnable_all', 'ofn_learnable_all');
+
+
+function ofn_learnable_as_html($title, $image, $category, $link) {
+    $html = '';
+
+    $html .= '<div class="ofn-learnable" style="background-image: url('.$image.');">'."\n";
+    $html .= '  <a href="'.$link.'">'."\n";
+    $html .= '    <div class="title">'.$title.'</div>'."\n";
+    $html .= "\n";
+    $html .= '    <div class="category category-'.strtolower($category).'">'."\n";
+    $html .= '      '.$category.''."\n";
+    $html .= '    </div>'."\n";
+    $html .= '  </a>'."\n";
+    $html .= '</div>'."\n";
+
+    return $html;
+}
